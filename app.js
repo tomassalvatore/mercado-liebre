60ed69b0817462f3521ea7da763b72603c25b312
Tomás Salvatore <tomisalvatore@gmail.com>
2021-11-04 15:30:09 -0300

    agregando puerto y procfile para deployar en Heroku

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.listen(3000);
+app.listen(process.env.PORT || 3000, function () {
+  console.log("Servidor corriendo en el puerto 3000");
+});
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "views/home.html"));
